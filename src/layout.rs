8024c45bad213421732b7ee3daa6af9df8899ffe
杉浦寛行 <r@rR.local>
2022-07-07 13:14:13 +0900

    Today's Lesson

--- src/layout.rs
+++ src/layout.rs
@@ -1,7 +1,8 @@
+#![allow(unused)]
 use crate::style;
 
 ///CSS box model. All sizes are in px.
-#[derive(Default,)]
+#[derive(Default, Clone,)]
 struct Dimensions {
    //Position of the content area  relative to the document origin:
    content: Rct,
@@ -12,7 +13,7 @@ struct Dimensions {
 }
 
 ///Rectangular module
-#[derive(Default,)]
+#[derive(Default, Clone,)]
 struct Rct {
    x:      f64,
    y:      f64,
@@ -21,7 +22,7 @@ struct Rct {
 }
 
 ///Positions of 4 corners
-#[derive(Default,)]
+#[derive(Default, Clone,)]
 struct EdgeSizes {
    left:   f64,
    right:  f64,
@@ -30,6 +31,7 @@ struct EdgeSizes {
 }
 
 ///The layout tree is a collection of layoutboxes. It contains boxes as child
+#[derive(Clone,)]
 struct LayoutBox<'a,> {
    box_type:   BoxType<'a,>,
    dimensions: Dimensions,
@@ -37,7 +39,9 @@ struct LayoutBox<'a,> {
 }
 
 ///A box can be a block node, an inline node, OR an anonymous block box
+#[derive(Clone,)]
 enum BoxType<'a,> {
+   Test(String,),
    BlockNode(&'a style::StyledNode<'a,>,),
    InlineNode(&'a style::StyledNode<'a,>,),
    AnonymousBlock,
@@ -50,16 +54,23 @@ impl<'a,> LayoutBox<'a,> {
    }
 
    ///Where a new inline child should go.
-   fn get_inline_container(mut self,) -> LayoutBox<'a,> {
-      match self.box_type {
+   fn get_inline_container(&'a mut self,) -> &'a mut LayoutBox<'a,> {
+      todo!(
+         "-----------------------------------------------------------
+             How about move self?
+             Own self's ownership and must return LayoutBox
+             ------------------------------------------------------------"
+      );
+      let ano = LayoutBox::new(BoxType::AnonymousBlock,);
+      match &self.box_type {
          BoxType::BlockNode(_,) => {
             //If we've just generated an anonymous block box, keep using it.
             //Otherwise, create a new one.
             match self.children.last() {
                Some(&LayoutBox { box_type: BoxType::AnonymousBlock, .. },) => {}
-               _ => self.children.push(LayoutBox::new(BoxType::AnonymousBlock,),),
+               _ => self.children.push(ano.clone(),),
             }
-            self.children.last().unwrap()
+            self.children.last_mut().unwrap()
          }
          _ => self,
       }
@@ -69,6 +80,11 @@ impl<'a,> LayoutBox<'a,> {
 ///Build the tree of LayoutBoxes,
 ///but don't perform any layout calculations yet.
 fn build_layout_tree<'a,>(style_node: &'a style::StyledNode<'a,>,) -> LayoutBox<'a,> {
+   todo!(
+      "----------------------------------------------------------
+          Use clone where borrow checker warning
+          -------------------------------------------------------------"
+   );
    use {style::Display::*, BoxType::*};
    //Create the root box.
    let mut root = LayoutBox::new(match style_node.display() {
@@ -78,10 +94,21 @@ fn build_layout_tree<'a,>(style_node: &'a style::StyledNode<'a,>,) -> LayoutBox<
    },);
    //Create the descendant boxes.
    for child in &style_node.children {
-      match child.display() {
-         Block | Inline => root.get_inline_container().children.push(build_layout_tree(&child,),),
-         _ => {}
-      }
+      root = match child.display() {
+         Block | Inline => {
+            let mut cl = root.clone();
+            cl.get_inline_container().children.push(build_layout_tree(&child,),);
+            cl.clone()
+         }
+         _ => root,
+      };
    }
    root
+}
+
+#[test]
+///This test's result was failed
+fn box_cl() {
+   let mut org = LayoutBox::new(BoxType::AnonymousBlock,);
+   org.clone().box_type = BoxType::Test(String::from("success",),);
 }
